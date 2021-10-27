de3116be4708a7986687d05629cb30e2319a5bba
catherine-tate <catherine21tate@gmail.com>
2021-10-27 17:45:29 -0400

    started revamping gallery to have small images that expand to full size on click

---
+++ files/js/gallery.js
@@ -0,0 +1,10 @@
+    
+
+function hideImage() {
+    document.getElementById("fullImage").style.display = "none";
+}
+
+function showImage() {
+    console.log("gfhfdh")
+    document.getElementById("fullImage").style.display = "block";
+}

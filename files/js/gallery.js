2c834ea055ff80d19c5a79bfc078d012d58e3324
catherine-tate <catherine21tate@gmail.com>
2021-10-28 16:19:14 -0400

    enlarged image frame now copies background color

--- files/js/gallery.js
+++ files/js/gallery.js
@@ -5,6 +5,11 @@ function hideImage() {
 }
 
 function showImage(image) {
-    document.getElementById("fullImage").style.display = "block";
-    document.getElementById("bigPic").src=image.children[0].src
+	console.log(image.id)
+    var bigFrame = document.getElementById("fullImage")
+    bigFrame.style.display = "block";
+    document.getElementById("bigPic").src=image.children[0].src;
+    var bgc = getComputedStyle(image)
+    bigFrame.style.backgroundColor = bgc.backgroundColor;
+
 }
